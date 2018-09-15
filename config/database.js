6e850e511edc1ede7dbb92d2185c3c7ee6a49449
Aluno <aluno@facimafaaiesa.local>
2018-09-15 16:52:30 -0300

    Api books complete

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoUrl = 'mongodb://davi:123125dv@ds257752.mlab.com:57752/curso-react-api';
+const mongoUrl = 'mongodb://davi:123dv@ds257752.mlab.com:57752/curso-react-api';
 
 const connect = () => mongoose.connect(mongoUrl);
 
